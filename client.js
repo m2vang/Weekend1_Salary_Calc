191595f9dc7828627509fc1e883b54dd620120f1
Mai Chee Vang <maicheevang@Mais-MacBook-Pro.local>
2018-07-28 18:19:19 -0500

    Completed Weekend1Challenge

--- client.js
+++ client.js
@@ -1,16 +1,40 @@
+let employee = [];
+let mSalary = [];
+
+class Employee{
+    constructor( name, lastName, idNumber, title, annualSalary){
+        this.name = name;
+        this.lastName = lastName;
+        this.idNumber = idNumber;
+        this.title = title;
+        this.annualSalary = annualSalary;
+    }// end of constructor
+}//end of Employee class
+
 $(document).ready(onReady);
 
 function onReady() {
-    //add table headers
-    $('#data').append(
-        `<tr>
-            <th>Name</th>
-            <th>Last Name</th>
-            <th>Id</th>
-            <th>Title</th>
-            <th>Annual Salary</th>
-        </tr>`
-    );
+    //create table
+    let table = $( '<table></table>');
+    table.append(`<thead><tr><th>Name</th>
+    <th>Last Name</th>
+    <th>Id Number</th>
+    <th>Title</th>
+    <th>Annual Salary</th>
+    <th id="remove"></th></thead>`);
+
+    //create table body
+    let tbody = $( '<tbody id="tablebody"></tbody>');
+    //add table body to table
+    table.append(tbody);
+
+    //create buttons
+    let submitBtn = $( '<button id="submit">Submit</button>');
+    //add buttons to buttons div
+    $('#sButton').append(submitBtn);
+
+    //add table to data div
+    $('#data').append(table);
 
     //run clickHandler
     clickHandler();
@@ -18,7 +42,13 @@ function onReady() {
 } //end of onReady
 
 function clickHandler() {
+    //adding employee
     $('#submit').on('click', addToTable);
+
+    //removing employee
+    //target parent of element that doesn't exist on DOM
+    if ($('#tablebody').on('click', '#row' , removeEmployee));
+
 } //end of clickHandler
 
 function addToTable() {
@@ -27,17 +57,47 @@ function addToTable() {
     let Id = $('#Id').val();
     let title = $('#title').val();
     let annualSalary = $('#annual-salary').val();
-    let addAnnSal = Math.round(annualSalary/12).toFixed(2);
+    let monthlySalary = Math.floor(annualSalary / 12);
+    //push the monthly salary into the mSalary array
+    mSalary.push(monthlySalary);
 
-    $('#data').append(
-        `<tr>
-            <td>${firstName}</td>
-            <td>${lastName}</td>
-            <td>${Id}</td>
-            <td>${title}</td>
-            <td>$${annualSalary}</td>
-            </tr>`
-    );
+    //create new employee & push it into the employee array
+    // let newEmployee = new Employee( $('#first-name').val(), $('#last-name').val(), 
+    // $('#Id').val(), $('#title').val(), $('#annual-salary').val() );
+    //line below is a better way to code for the same two lines above
+    employee.push(new Employee ($('#first-name').val(), $('#last-name').val(),
+        $('#Id').val(), $('#title').val(), $('#annual-salary').val()) );
+    
+    //adding value input to the table body
+    $('#tablebody').append('<tr id="row"><td>' + firstName + 
+        '</td><td>' + lastName +
+        '</td><td>' + Id + 
+        '</td><td>' + title + 
+        '</td><td>' + '$' + annualSalary + 
+        '</td><td><button id="delete">Delete</button></td>'  +
+        '</tr>');
 
-    $('.monthlySal').html('Total Monthly: $' + addAnnSal);
+    // $('#tablebody').append(deleteBtn);
+    //change total monthly background color to red if mSalary > 20000
+    $('.monthlySal').html('Total Monthly: $' + mSalary.reduce(getSum).toFixed(2));
+    if (mSalary.reduce(getSum).toFixed(2) > 20000){
+        $('#footer').css('background-color', 'red');
+    }
+
+    //empty each input
+    $('#first-name').val('');
+    $('#last-name').val('');
+    $('#Id').val('');
+    $('#title').val('');
+    $('#annual-salary').val('');
 } //end of addToTable
+
+function getSum(total, num) {
+    return total + num;
+    
+}
+
+function removeEmployee() {
+    $('#row').remove();
+
+}
